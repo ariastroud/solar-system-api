9d692913f94e81443b21971a5901785691bde09f
dainiz <dainiz.almazan@gmail.com>
2022-10-21 11:07:55 -0700

    Creates Planet class, planets list, and planet endpoint.

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,31 @@
-from flask import Blueprint
+from unicodedata import name
+from flask import Blueprint, jsonify
 
+class Planet:
+
+    def __init__(self, id, name, description, size):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.size = size
+
+planets = [
+    Planet(1, "mercury", "the first planet", "small"),
+    Planet(2, "earth", "our planet", "medium"),
+    Planet(3, "jupiter", "a large planet", "large")
+]
+
+planet_bp = Blueprint("planet", __name__, url_prefix="/planets")
+
+@planet_bp.route("", methods=["GET"])
+def handle_planets():
+    planets_list = []
+    for planet in planets:
+        planets_list.append(dict
+            (
+            id=planet.id,
+            name=planet.name,
+            description=planet.description,
+            size=planet.size
+            )
+        )
