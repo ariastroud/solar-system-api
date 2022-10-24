05a269a98cfaa538530096a56e8634688998f248
Aria Stroud <stroudaria@gmail.com>
2022-10-24 11:47:29 -0700

    Adds endpoint for one planet

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from unicodedata import name
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 class Planet:
 
@@ -8,6 +8,14 @@ class Planet:
         self.name = name
         self.description = description
         self.size = size
+    
+    def to_json(self):
+        return dict(
+            id=self.id,
+            name=self.name,
+            description=self.description,
+            size=self.size
+            )
 
 planets = [
     Planet(1, "mercury", "the first planet", "small"),
@@ -21,12 +29,21 @@ planet_bp = Blueprint("planet", __name__, url_prefix="/planets")
 def handle_planets():
     planets_list = []
     for planet in planets:
-        planets_list.append(dict
-            (
-            id=planet.id,
-            name=planet.name,
-            description=planet.description,
-            size=planet.size
-            )
-        )
+        planets_list.append(planet.to_json())
     return jsonify(planets_list)
+
+@planet_bp.route("/<id>", methods=["GET"])
+def handle_planet(id):
+    planet = validate_planet(id)
+    return jsonify(planet.to_json())
+
+def validate_planet(id):
+    try:
+        planet_id = int(id)
+    except:
+        abort(make_response({"message": f"planet {id} invalid"}, 400))
+
+    for planet in planets:
+        if planet_id == planet.id:
+            return planet
+    abort(make_response({"message": f"planet {id} not found"}, 404))
