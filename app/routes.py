4a3d77c412234dcc0180c98f709dcda70848f0af
dainiz <dainiz.almazan@gmail.com>
2022-11-01 10:40:30 -0700

    Adds GET method for one planet

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,24 @@ from flask import Blueprint, jsonify, abort, make_response, request
 
 planet_bp = Blueprint("planet", __name__, url_prefix="/planets")
 
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except:
+        abort(make_response({"message": f"{planet_id} is not a valid planet id"}, 400))
+
+    planet = Planet.query.get(planet_id)
+    if not planet:
+        abort(make_response({"message": f"{planet_id} not found"}, 404))
+    
+    return planet
+
+@planet_bp.route("/<planet_id>", methods=["GET"])
+def handle_planet(planet_id):
+    planet = validate_planet(planet_id)
+
+    return jsonify(planet.to_json()), 200
+
 @planet_bp.route("", methods=["POST"])
 def create_planet():
     request_body = request.get_json()
