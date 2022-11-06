83c1a3b98e0d98c0a978f08278930119ecc2c85a
Aria Stroud <stroudaria@gmail.com>
2022-11-05 19:00:32 -0700

    Add validate_model, create_moon and read_all_moons to moon_routes.py

--- app/routes/moon_routes.py
+++ app/routes/moon_routes.py
@@ -3,3 +3,37 @@ from app.models.moon import Moon
 from flask import Blueprint, jsonify, abort, make_response, request
 
 moon_bp = Blueprint("moon", __name__, url_prefix="/moons")
+
+def validate_model(cls, model_id):
+    try:
+        model_id = int(model_id)
+    except:
+        abort(make_response(
+            {"message": f"{cls.__name__} {model_id} invalid"}, 400))
+
+    model = cls.query.get(model_id)
+    if not model:
+        abort(make_response(
+            {"message": f"{cls.__name__} {model_id} not found"}, 404))
+
+    return model
+
+
+@moon_bp.route("", methods=["POST"])
+def create_moon():
+    request_body = request.get_json()
+    new_moon = Moon.from_dict(request_body)
+
+    db.session.add(new_moon)
+    db.session.commit()
+
+    return make_response(f"Moon {new_moon.name} successfully created", 201)
+
+
+@moon_bp.route("", methods=["GET"])
+def read_all_moons():
+    moons = Moon.query.all()
+
+    moons_response = [moon.to_dict() for moon in moons]
+
+    return jsonify(moons_response)
