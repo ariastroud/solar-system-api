bc8de177e6f0a78607f2f2cbd654f81c2f5a8906
Aria Stroud <stroudaria@gmail.com>
2022-11-05 19:10:26 -0700

    Adds nested route for /planets/<planet_id>/moons with the POST method

--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -1,5 +1,6 @@
 from app import db
 from app.models.planet import Planet
+from app.models.moon import Moon
 from flask import Blueprint, jsonify, abort, make_response, request
 
 planet_bp = Blueprint("planet", __name__, url_prefix="/planets")
@@ -71,3 +72,16 @@ def delete_planet(planet_id):
     db.session.commit()
 
     return make_response(f"Planet {planet.id} successfully deleted"), 200
+
+# create a Moon and link it to an existing Planet record
+@planet_bp.route("/<planet_id>/moons", methods=["POST"])
+def create_moon(planet_id):
+    planet = validate_model(Planet, planet_id)
+    request_body = request.get_json()
+    new_moon = Moon.from_dict(request_body)
+    new_moon.planet = planet
+
+    db.session.add(new_moon)
+    db.session.commit()
+
+    return make_response(f"Moon {new_moon.name} is a moon of {planet.name}", 201)
